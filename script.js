f84efb673c97c1ab82a515a50e1844ad5c63e954
Gummigrodan <isacericlarsson@outlook.com>
2026-06-20 16:48:17 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const images = Array.from(
+    { length: 50 },
+    (_, i) => `static/reddit_images/${i + 1}.webp`
+);
+
+
+function randomizeImage() {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    const imgElement = document.getElementById("randomImage");
+
+    imgElement.src = images[randomIndex];
+}
+
+randomizeImage();
